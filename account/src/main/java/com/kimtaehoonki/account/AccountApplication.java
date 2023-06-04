c3ab9b38fc729e255b9b468b1a606177677ab463
fpg123 <iui4793@gmail.com>
2023-06-04 12:19:52 +0900

    feat(account): 시큐리티 기능 끄기

--- account/src/main/java/com/kimtaehoonki/account/AccountApplication.java
+++ account/src/main/java/com/kimtaehoonki/account/AccountApplication.java
@@ -2,8 +2,9 @@ package com.kimtaehoonki.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AccountApplication {
 
 	public static void main(String[] args) {
