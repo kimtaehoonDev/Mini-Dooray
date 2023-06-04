adb3c7e934c7ad9445c0776fc163417c52d014ac
honki12345 <iui4793@gmail.com>
2023-06-04 20:56:10 +0900

    feat(task): TagController method signature

--- task/src/main/java/com/kimtaehoonki/task/tag/presentation/controller/TagController.java
+++ task/src/main/java/com/kimtaehoonki/task/tag/presentation/controller/TagController.java
@@ -1,4 +1,35 @@
 package com.kimtaehoonki.task.tag.presentation.controller;
 
+import com.kimtaehoonki.task.tag.presentation.dto.DeleteTagResponseDto;
+import com.kimtaehoonki.task.tag.presentation.dto.RegisterTagRequestDto;
+import com.kimtaehoonki.task.tag.presentation.dto.RegisterTagResponseDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TagController {
+    /**
+     * 태그를 등록한다.
+     *
+     * @param requestDto @RequestBody
+     * @return RegisterTagResponseDto
+     */
+    @PostMapping("/tags")
+    public RegisterTagResponseDto registerTag(@RequestBody RegisterTagRequestDto requestDto) {
+        return null;
+    }
+
+    /**
+     * 해당 태그를 삭제한다.
+     *
+     * @param tagId @PathVariable
+     * @return DeleteTagResponseDto
+     */
+    @DeleteMapping("/tags/{id}")
+    public DeleteTagResponseDto deleteTag(@PathVariable("id") Long tagId) {
+        return null;
+    }
 }
---
+++ task/src/main/java/com/kimtaehoonki/task/tag/presentation/dto/DeleteTagResponseDto.java
@@ -0,0 +1,4 @@
+package com.kimtaehoonki.task.tag.presentation.dto;
+
+public class DeleteTagResponseDto {
+}
