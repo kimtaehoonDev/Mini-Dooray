4930ebc4cc9d164c91e4a9c86b2f6eee801b5764
kimtaehoonDev <thkimm.dev@gmail.com>
2023-06-05 09:47:56 +0900

    feat(account): LoginRequestDto AllArgsConstructor 생성

--- account/src/main/java/com/kimtaehoonki/account/presentation/dto/LoginRequestDto.java
+++ account/src/main/java/com/kimtaehoonki/account/presentation/dto/LoginRequestDto.java
@@ -1,11 +1,12 @@
 package com.kimtaehoonki.account.presentation.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginRequestDto {
     private String username;
     private String password;
