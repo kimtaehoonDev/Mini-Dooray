cbd7740a4a34ca64a39d065cb7635d69d5e20438
kimtaehoonDev <thkimm.dev@gmail.com>
2023-06-11 14:11:44 +0900

    test(task): ProjectRepository 테스트 케이스 작성

---
+++ task/src/test/java/com/kimtaehoonki/task/project/domain/ProjectRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.kimtaehoonki.task.project.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.kimtaehoonki.task.TestConfig;
+import com.kimtaehoonki.task.project.application.dto.response.ProjectDetail;
+import com.kimtaehoonki.task.project.domain.entity.Project;
+import java.lang.reflect.Field;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("dev")
+@Import(TestConfig.class)
+class ProjectRepositoryTest {
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Test
+    void 존재하는_프로젝트를_이름으로_조회하면_true를_반환한다() {
+        Project project = Project.make(1, "Hello", null);
+        projectRepository.save(project);
+
+        boolean result = projectRepository.existsByName("Hello");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 존재하지_않는_프로젝트를_이름으로_조회하면_false를_반환한다() {
+        Project project = Project.make(1, "Hello", null);
+        projectRepository.save(project);
+
+        boolean result = projectRepository.existsByName("Jello2");
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 아이디로_조회할수_있다() throws NoSuchFieldException, IllegalAccessException {
+        Project project = Project.make(1, "hello", null);
+
+        Class<Project> projectClazz = Project.class;
+        Field idField = projectClazz.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(project, 1L);
+        projectRepository.save(project);
+
+        ProjectDetail projectDetail = projectRepository.findById(1L, ProjectDetail.class)
+            .get();
+        assertThat(projectDetail.getName()).isEqualTo("hello");
+    }
+
+    @Test
+    void 존재하지_않는_아이디로_조회하면_비어있는_Optional객체를_반환한다() {
+        Optional<ProjectDetail> projectDetailOpt = projectRepository.findById(1L, ProjectDetail.class);
+        assertThat(projectDetailOpt).isEmpty();
+    }
+}
