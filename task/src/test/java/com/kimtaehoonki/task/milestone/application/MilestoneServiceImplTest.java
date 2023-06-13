47bcbbd2c4b316b29fd69deb7e24dff0183512cb
kimtaehoonDev <thkimm.dev@gmail.com>
2023-06-13 15:20:44 +0900

    fix(task): ColorGenerator Component 등록하지 않은 이슈 해결

--- task/src/test/java/com/kimtaehoonki/task/milestone/application/MilestoneServiceImplTest.java
+++ task/src/test/java/com/kimtaehoonki/task/milestone/application/MilestoneServiceImplTest.java
@@ -24,8 +24,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("dev")
 class MilestoneServiceImplTest {
     @Mock
     MilestoneRepository milestoneRepository;
